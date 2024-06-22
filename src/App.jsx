7e92aa27c2cb731b391b860ca87988f0403613f7
karunlal <karunlalp@gmail.com>
2024-06-22 19:48:44 +0530

    added useEffect and opertions to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
+  }
+
+  const updateTodo = () => (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo))
+    )
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    )
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem('todos'))
+    if (todos && todos.length > 0) {
+      setTodos()
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
   return (
-    <>
-      <div>Hello world</div>
-    </>
+    <TodoProvider
+      value={(todos, addTodo, updateTodo, deleteTodo, toggleComplete)}
+    >
+      <>Hello world</>
+    </TodoProvider>
   )
 }
 
